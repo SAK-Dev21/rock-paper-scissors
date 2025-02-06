2763440d41ebe5b7539f339585fbf46be73b8da6
SahilCov <ahmedkhans@coventry.ac.uk>
2025-02-06 21:42:12 +0000

    Randomly generate values for rock, paper and scissors
    By randomly generating an outcome inside the function getComputerChoice
    (), it is easy to collect different outcomes by just calling the
    function on one line.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello world!")
+function getComputerChoice()
+{
+    // NOTE: I have used console.log() instead of normal returns as this is an early version of this project. V1 will orient around the console and V2 will utilise a GUI more.
+
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+        case 1:
+            console.log('rock');
+            break;
+        case 2:
+            console.log('paper');
+            break;
+        case 3:
+            console.log('scissors')
+            break;
+    }
+}
+
+getComputerChoice()
