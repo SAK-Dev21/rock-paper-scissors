ea35346775b637964ef094d9273aba848e4d8140
SahilCov <ahmedkhans@coventry.ac.uk>
2025-02-06 23:36:51 +0000

    Create 5 rounds of rock, paper, scissors
    By declaring the score variables and playRound() function inside the
    scope of the playGame() function, and running the playRound() function
    in a for loop, it makes accessing code more orderly as it is all in
    one place.
    
    Creating 5 rounds also allows the game to run for a bit longer too.
    
    Keep

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // NOTE: I have used console.log() alongside return statements for testing.
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     switch (randomNumber) {
@@ -24,36 +21,60 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    const outcome = humanChoice + computerChoice;
 
-    switch (outcome) {
-        case 'rockrock':
-        case 'paperpaper':
-        case 'scissorsscissors':
-            console.log('Tie.');
-            return 'Tie.';                             
-        case 'rockpaper':
-        case 'paperscissors':
-        case 'scissorsrock':
-            console.log(`Computer wins! ${computerChoice} beats ${humanChoice}!`);
-            computerScore += 1;
-            return `Computer wins! ${computerChoice} beats ${humanChoice}!`;
-        case 'rockscissors':
-        case 'paperrock':
-        case 'scissorspaper':
-            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-            humanScore += 1;
-            return `You win! ${humanChoice} beats ${computerChoice}!`;
-        
-        default:
-            console.log('Invalid game state.');
-            return 'Invalid game state.';
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        const outcome = humanChoice + computerChoice;
+    
+        switch (outcome) {
+            case 'rockrock':
+            case 'paperpaper':
+            case 'scissorsscissors':
+                console.log('Tie.');
+                return 'Tie.';                             
+            case 'rockpaper':
+            case 'paperscissors':
+            case 'scissorsrock':
+                console.log(`Computer wins! ${computerChoice} beats ${humanChoice}!`);
+                computerScore += 1;
+                return `Computer wins! ${computerChoice} beats ${humanChoice}!`;
+            case 'rockscissors':
+            case 'paperrock':
+            case 'scissorspaper':
+                console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+                humanScore += 1;
+                return `You win! ${humanChoice} beats ${computerChoice}!`;
+            
+            default:
+                console.log('Invalid game state.');
+                return 'Invalid game state.';
+        }
+    
+    }
+
+    for (let index = 0; index < 5; index++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);               
+    }
+    
+    console.log(`Total player score: ${humanScore}`)
+    console.log(`Total computer score: ${computerScore}`)
+
+    if (humanScore > computerScore) {
+        console.log('Player wins!')
+        return 'Player wins!';
+    } else if (computerScore > humanScore) {
+        console.log('Computer wins!')
+        return 'Computer wins!';
+    } else {
+        console.log("It's a tie!")
+        return "It's a tie!";
     }
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
