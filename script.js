a80780382ac89107c7669867501fe44d3df286d6
SahilCov <ahmedkhans@coventry.ac.uk>
2025-02-06 21:56:50 +0000

    Create a human input for entering values
    Creating a human input allows for the user to compete against the
    computer for rock paper scissors. By putting the input inside function
    getHumanChoice(), input can be called by just calling the function.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function getComputerChoice()
-{
-    // NOTE: I have used console.log() instead of normal returns as this is an early version of this project. V1 will orient around the console and V2 will utilise a GUI more.
+// NOTE: I have used console.log() instead of normal returns as this is an early version of this project. V1 will orient around the console and V2 will utilise a GUI more.
 
+function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3) + 1;
-
     switch (randomNumber) {
         case 1:
             console.log('rock');
@@ -17,4 +15,9 @@ function getComputerChoice()
     }
 }
 
-getComputerChoice()
+function getHumanChoice() {
+    choice = prompt("Rock, Paper or Scissors? (Enter in all lower case)", "Enter in all lower case.");
+    console.log(choice);
+}
+
+getHumanChoice()
